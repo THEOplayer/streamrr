a3b2fb3928c1ff49b2b38edda41b2b74054ba08f
Mattias Buelens <mattias.buelens@dolby.com>
2025-11-03 13:39:10 +0100

    Parse HAR content

--- src/import/har.rs
+++ src/import/har.rs
@@ -1,5 +1,6 @@
 pub use har::v1_3::*;
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 #[serde(tag = "version")]
@@ -26,4 +27,70 @@ pub enum Spec {
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct Har {
     pub log: Spec,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Default)]
+pub struct Log {
+    pub creator: Creator,
+    pub browser: Option<Creator>,
+    pub pages: Option<Vec<Pages>>,
+    pub entries: Vec<Entries>,
+    pub comment: Option<String>,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Default)]
+pub struct Entries {
+    pub pageref: Option<String>,
+    #[serde(rename = "startedDateTime")]
+    pub started_date_time: String,
+    pub time: f64,
+    pub request: Request,
+    pub response: Response,
+    pub cache: Cache,
+    pub timings: Timings,
+    #[serde(rename = "serverIPAddress")]
+    pub server_ip_address: Option<String>,
+    pub connection: Option<String>,
+    pub comment: Option<String>,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Default)]
+pub struct Response {
+    pub status: i64,
+    #[serde(rename = "statusText")]
+    pub status_text: String,
+    #[serde(rename = "httpVersion")]
+    pub http_version: String,
+    pub cookies: Vec<Cookies>,
+    pub headers: Vec<Headers>,
+    pub content: Content,
+    #[serde(rename = "redirectURL")]
+    pub redirect_url: Option<String>,
+    #[serde(rename = "headersSize", default = "default_isize")]
+    pub headers_size: i64,
+    #[serde(rename = "bodySize", default = "default_isize")]
+    pub body_size: i64,
+    pub comment: Option<String>,
+    #[serde(rename = "headersCompression")]
+    pub headers_compression: Option<i64>,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Default)]
+pub struct Content {
+    #[serde(default = "default_isize")]
+    pub size: i64,
+    pub compression: Option<i64>,
+    #[serde(rename = "mimeType")]
+    pub mime_type: Option<String>,
+    pub text: Option<String>,
+    pub encoding: Option<String>,
+    pub comment: Option<String>,
+}
+
+fn default_isize() -> i64 {
+    -1
 }
